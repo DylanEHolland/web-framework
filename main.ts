001e5d58783c8fd239b88fe27ee051507fd7da4c
DylanEHolland <salinson1138@gmail.com>
2021-12-16 17:38:56 -0800

    left out some things

--- main.ts
+++ main.ts
@@ -7,13 +7,6 @@ const sampleMiddleware = (req: Request, res: Response) => {
   console.log("before send");
 };
 
-app.get("/:id", (req, res) => {
-  console.log(req.params);
-  res.status(200).send(`id: ${req.params.id}`);
-});
 
-app.use(sampleMiddleware, (req, res) => {
-  res.status(200).send({ status: "Good" });
-});
 
 app.listen(3500);
---
+++ src/srv/api/index.ts
@@ -0,0 +1,14 @@
+// app.get("/:id", (req, res) => {
+//     console.log(req.params);
+//     res.status(200).send(`id: ${req.params.id}`);
+// });
+  
+// app.use(sampleMiddleware, (req, res) => {
+//     res.status(200).send({ status: "Good" });
+// });
+
+function add_route(path, response) {
+
+}
+
+add_route("/", "<b>this is a test</b>");
